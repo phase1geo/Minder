ee4b99080c8e7bb33d9b22f61438361684840d13
Trevor Williams <phase1geo@gmail.com>
2018-02-22 01:28:53 -0600

    Updating files

--- po/cmake_install.cmake
+++ po/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "/usr")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
