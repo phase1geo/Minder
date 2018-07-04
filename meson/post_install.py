11abbe0078bfa3d20551cea1213231fd447e5822
Trevor Williams <phase1geo@gmail.com>
2018-07-03 22:51:32 -0500

    * Adding call to gtk-update-icon-cache after installing icons.

--- meson/post_install.py
+++ meson/post_install.py
@@ -8,3 +8,8 @@ schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0'
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir], shell=False)
+
+# Make sure that the icons are properly installed
+print('Updating icon cache...')
+hicolor = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
+subprocess.call(['gtk-update-icon-cache', '-f', '-t', hicolor], shell=False)
