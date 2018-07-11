7fcb0a4fbfb994cc8abe06b893da2366fb72e97c
Trevor Williams <phase1geo@gmail.com>
2018-07-10 19:50:02 -0500

    * Attempting to correct Travis CI failures.

--- meson/post_install.py
+++ meson/post_install.py
@@ -3,13 +3,12 @@
 import os
 import subprocess
 
-schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+schemadir  = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+hicolordir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir], shell=False)
 
-# Make sure that the icons are properly installed
-print('Updating icon cache...')
-hicolor = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
-subprocess.call(['gtk-update-icon-cache', '-f', '-t', hicolor], shell=False)
+    print('Updating icon cache...')
+    subprocess.call(['gtk-update-icon-cache', hicolordir], shell=False)
