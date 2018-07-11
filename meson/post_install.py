2e3cce3e729d1a4c153ac02a098f9a5c5b88394b
Trevor Williams <phase1geo@gmail.com>
2018-07-10 22:13:49 -0500

    * Update.

--- meson/post_install.py
+++ meson/post_install.py
@@ -3,12 +3,11 @@
 import os
 import subprocess
 
-schemadir  = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
-hicolordir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
-
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
+    schemadir  = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
     subprocess.call(['glib-compile-schemas', schemadir], shell=False)
 
-    # print('Updating icon cache...')
-    # subprocess.call(['gtk-update-icon-cache', hicolordir], shell=False)
+    print('Updating icon cache...')
+    hicolordir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
+    subprocess.call(['gtk-update-icon-cache', hicolordir], shell=False)
