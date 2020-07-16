1e437ef194ab59c10332e73b07ebe9a9597769d0
Trevor Williams <phase1geo@gmail.com>
2020-07-16 00:45:17 -0500

    Importing flat-color-icons repository as a gresource.

---
+++ data/bin/gen-icon-gresource.tcl
@@ -0,0 +1,7 @@
+# Usage:  tclsh8.6 bin/gen-icon-gresource.tcl  (run from 'data' directory)
+
+set svg_dir [file join icons flat-color-icons svg]
+
+foreach item [glob -nocomplain -directory $svg_dir -tails *.svg] {
+  puts "    <file alias=\"$item\">[file join $svg_dir $item]</file>"
+}
